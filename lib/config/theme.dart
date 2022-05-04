97f457658eec674bd25f012fe433a9043e99e388
Isabella McGinley <isabellabitt1@hotmail.com>
2022-05-04 09:31:17 -0300

    theme and myapp separation done

---
+++ lib/config/theme.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+ThemeData theme() {
+  return ThemeData(
+    fontFamily: GoogleFonts.imFellGreatPrimer().fontFamily,
+    textTheme: const TextTheme(
+      headline1: TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.normal,
+        fontSize: 32,
+      ),
+      headline2: TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.normal,
+        fontSize: 24,
+      ),
+      headline3: TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.normal,
+        fontSize: 20,
+      ),
+      headline4: TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.normal,
+        fontSize: 16,
+      ),
+      headline5: TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.normal,
+        fontSize: 14,
+      ),
+      headline6: TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.normal,
+        fontSize: 14,
+      ),
+    ),
+  );
+}
---
+++ lib/my_app.dart
@@ -0,0 +1,16 @@
+import 'package:api_series/config/theme.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return 
+      MaterialApp(
+        title: 'Show Analytic',
+        theme: theme(),
+        debugShowCheckedModeBanner: false,
+      );
+  }
+}
