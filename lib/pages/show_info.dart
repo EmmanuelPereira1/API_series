123945e11344c1f487c958fa027a5a1098f19715
Isabella McGinley <isabellabitt1@hotmail.com>
2022-05-05 16:02:08 -0300

    info_show_page in progress

--- lib/pages/show_info.dart
+++ lib/pages/show_info.dart
@@ -1,3 +1,4 @@
+import 'package:api_series/config/theme.dart';
 import 'package:flutter/material.dart';
 
 import '../request/show_list.dart';
@@ -30,37 +31,144 @@ class ShowInfo extends StatefulWidget {
 class _ShowInfoState extends State<ShowInfo> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.transparent,
-        body: NestedScrollView(
-            floatHeaderSlivers: true,
-            headerSliverBuilder: (context, innerBoxIsScrolled) =>
-                [const CustomAppBar()],
-            body: Column(
-              children: [
-                Container(
-                  margin: EdgeInsets.fromLTRB(30, 0, 30, 350),
-                  width: 400,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: NetworkImage(widget.imageThumbnailPath)),
-                  ),
-                  child: Row(
+    return Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: <Color>[
+              Color(0XFF101A26),
+              Color(0XFF026873),
+              Color(0xFF35F2DF)
+            ])),
+        child: Scaffold(
+            backgroundColor: Colors.transparent,
+            body: NestedScrollView(
+                floatHeaderSlivers: true,
+                headerSliverBuilder: (context, innerBoxIsScrolled) =>
+                    [const CustomAppBar()],
+                body: SingleChildScrollView(
+                    child: Stack(
                   children: [
-                    Text(widget.status, style: Theme.of(context).textTheme.headline4!.copyWith(fontWeight: FontWeight.bold),),
-                    SizedBox(width: 5,),
-                    Icon(Icons.circle, size: 5, color: Colors.white),
-                    SizedBox(width: 5,),
-                    Text(widget.country, style: Theme.of(context).textTheme.headline4!.copyWith(fontWeight: FontWeight.bold),),
-                    SizedBox(width: 5,),
-                    Icon(Icons.circle, size: 5, color: Colors.white),
-                    SizedBox(width: 5,),
-                    Text(widget.network, style: Theme.of(context).textTheme.headline4!.copyWith(fontWeight: FontWeight.bold),),
+                    // AspectRatio(
+                    //     aspectRatio: 400 / 451,
+                    //     child: Container(
+                    //       decoration: BoxDecoration(
+                    //         image: DecorationImage(
+                    //             image: NetworkImage(widget.imageThumbnailPath),
+                    //             fit: BoxFit.fitWidth,
+                    //             ),
+                    //       ),
+
+                    //     )),
+                    AspectRatio(
+                      aspectRatio: 400 / 451,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: NetworkImage(widget.imageThumbnailPath),
+                                fit: BoxFit.fitWidth,
+                                ),
+                          ),
+                      ),
+                    ),
+                    Container(
+                      height: 410,
+                      width: 451,
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.center,
+                          end: Alignment.topCenter,
+                          colors: [
+                            Color(0xCC000000),
+                            Color(0x00000000),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(10, 365, 30, 2),
+                      child: Row(
+                        children: [
+                          Text(widget.status,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline3!
+                                  .copyWith(fontWeight: FontWeight.bold)),
+                          const SizedBox(width: 5),
+                          const Icon(Icons.circle, color: Colors.white, size: 6),
+                          const SizedBox(width: 5),
+                          Text(widget.country,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline3!
+                                  .copyWith(fontWeight: FontWeight.bold)),
+                          const SizedBox(width: 10),
+                          Text(widget.network,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline3!
+                                  .copyWith(fontWeight: FontWeight.bold)),
+                        ],
+                      ),
+                    )
                   ],
-                ),
-                ),
-              ],
-            )));
+                )))));
   }
 }
+                              // child: Scaffold(
+                              //     backgroundColor: Colors.transparent,
+                              //     body: NestedScrollView(
+                              //         floatHeaderSlivers: true,
+                              //         headerSliverBuilder: (context, innerBoxIsScrolled) =>
+                              //             [const CustomAppBar()],
+                              //         body: SingleChildScrollView(
+                              //           child: AspectRatio(
+                              //             aspectRatio: 400 / 451,
+                              //             child: Container(
+                              //               decoration: BoxDecoration(
+                              //                 image: DecorationImage(
+                              //                     image: NetworkImage(widget.imageThumbnailPath),
+                              //                     fit: BoxFit.fitWidth),
+                              //               ),
+                              //               child: Container(
+                              //                 color: Colors.black,
+                              //                 child: Row(
+                              //                   mainAxisAlignment: MainAxisAlignment.start,
+                              //                   crossAxisAlignment: CrossAxisAlignment.end,
+                              //                   children: [
+                              //                     const SizedBox(width: 10),
+                              //                     Text(
+                              //                       widget.status,
+                              //                       style: Theme.of(context)
+                              //                           .textTheme
+                              //                           .headline2!
+                              //                           .copyWith(fontWeight: FontWeight.bold),
+                              //                     ),
+                              //                     const SizedBox(
+                              //                       width: 5,
+                              //                     ),
+                              //                     Text(
+                              //                       widget.country,
+                              //                       style: Theme.of(context)
+                              //                           .textTheme
+                              //                           .headline2!
+                              //                           .copyWith(fontWeight: FontWeight.bold),
+                              //                     ),
+                              //                     const SizedBox(
+                              //                       width: 5,
+                              //                     ),
+                              //                     Text(
+                              //                       widget.network,
+                              //                       style: Theme.of(context)
+                              //                           .textTheme
+                              //                           .headline2!
+                              //                           .copyWith(fontWeight: FontWeight.bold),
+                              //                     ),
+                              //                   ],
+                              //                 ),
+                              //               ),
+                              //             ),
+                              //           ),
+                              //         ))),
+               
