f971a5b85786c74221d6b870baa40241f3f9d705
EmmanuelPereira1 <97740932+EmmanuelPereira1@users.noreply.github.com>
2022-05-12 15:40:09 -0300

    name_show_ok

--- lib/pages/favorite_page.dart
+++ lib/pages/favorite_page.dart
@@ -53,7 +53,7 @@ class _FavoritePageState extends State<FavoritePage> {
                   child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        SizedBox(height: 20),
+                        const SizedBox(height: 20),
                         Card(
                           color: Colors.white,
                           elevation: 4,
@@ -65,34 +65,38 @@ class _FavoritePageState extends State<FavoritePage> {
                               margin: const EdgeInsets.all(10.0),
                               elevation: 0,
                               child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
                                 children: [
                                   const SizedBox(height: 10),
-                                  Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
                                     children: [
-                                      Text(document['first_name'],
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .headline4!
-                                              .copyWith(
-                                                  color: Colors.black,
-                                                  fontWeight: FontWeight.bold)),
-                                      RatingBarIndicator(
-                                        rating: document['rating'],
-                                        itemBuilder: (context, index) => const Icon(
-                                          Icons.star,
-                                          color: Colors.amber,
-                                        ),
-                                        itemCount: 5,
-                                        itemSize: 20.0,
-                                        direction: Axis.horizontal,
+                                      
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Text(document['first_name'],
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .headline4!
+                                                  .copyWith(
+                                                      color: Colors.black,
+                                                      fontWeight: FontWeight.bold)),
+                                          RatingBarIndicator(
+                                            rating: document['rating'],
+                                            itemBuilder: (context, index) => const Icon(
+                                              Icons.star,
+                                              color: Colors.amber,
+                                            ),
+                                            itemCount: 5,
+                                            itemSize: 20.0,
+                                            direction: Axis.horizontal,
+                                          ),
+                                        ],
                                       ),
-                                    ],
-                                  ),
-                                  Text(
+                                      Text(
                                     document['review_text'],
                                     style: Theme.of(context)
                                         .textTheme
@@ -101,6 +105,16 @@ class _FavoritePageState extends State<FavoritePage> {
                                           color: Colors.grey[600],
                                         ),
                                   ),
+                                     
+                                    ],
+                                  ),
+                                   Text(document['show_name'],
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .headline5!
+                                                  .copyWith(
+                                                      color: Colors.black,
+                                                      fontWeight: FontWeight.bold)),
                                 ],
                               ),
                             ),
